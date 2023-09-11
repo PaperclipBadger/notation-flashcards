f2a09bfc96f640481aa45a8b81e6fef3afa4c70e
Blaine Rogers <blaine.w.rogers@gmail.com>
2023-09-11 23:06:28 +0900

    Changes, yay!

--- notation_flashcards/__main__.py
+++ notation_flashcards/__main__.py
@@ -8,7 +8,7 @@ import tqdm
 from notation_flashcards.theory import (
     ChordKind, KeySignature, Mode, Note, accordion_chord, circle, invert
 )
-from notation_flashcards.render import render_chord
+from notation_flashcards.render import ScoreRenderer
 
 
 pathlib.Path('images').mkdir(exist_ok=True)
@@ -16,6 +16,27 @@ pathlib.Path('images').mkdir(exist_ok=True)
 deck = genanki.Deck(deck_id=1381930778, name='Accordion Chords')
 
 nids = iter(itertools.count())
+
+note_model = genanki.Model(
+    model_id=1228304155,
+    name='note',
+    fields=[
+        dict(name='NID'),
+        dict(name='Key Signature'),
+        dict(name='Note Name'),
+        dict(name='Octave'),
+        dict(name='Score'),
+    ],
+    templates=[
+        dict(
+            name='recall',
+            qfmt='{{Score}}',
+            afmt='{{Score}}<hr id="answer"><span style="font-size: 20px">{{Note Name}}</span><br>key signature: {{Key Signature}}<br>octave: {{Octave}}',
+        ),
+    ]
+)
+
+deck.add_model(note_model)
 
 chord_model = genanki.Model(
     model_id=1330235254,
@@ -54,36 +75,65 @@ keysigs = itertools.chain(
 
 roots = [circle(i) for i in itertools.chain(range(0, 6), range(-6, 0))]
 
-iterable = itertools.product(keysigs, roots, (0, 1, 2), ('bass', 'treble'), ChordKind)
-total = 15 * 12 * len(ChordKind) * 3 * 2
+for keysig in tqdm.tqdm(keysigs, total=15, desc="Key Signatures"):
+    for note in tqdm.trange(-24, 24, leave=False, desc="Notes"):
+        octave = note // 12
 
-for keysig, root, inversion, staff, kind in tqdm.tqdm(iterable, total=total):
-    nid = format(next(nids), '06d')
+        nid = format(next(nids), '06d')
 
-    chord = accordion_chord(root, kind)
-    if staff == 'treble':
-        chord = tuple(note + 24 for note in chord)
+        media_path = f'images/score_{nid}.png'
+        media_files.append(media_path)
+        staff = 'bass' if octave < 0 else 'treble'
 
-    chord_name = kind.name(root)
+        with ScoreRenderer(keysig, media_path) as renderer:
+            renderer.add_note(note, staff)
 
-    media_path = f'images/score_{nid}.png'
-    media_files.append(media_path)
-    render_chord(keysig, invert(inversion, chord), staff, path=media_path)
+        note = genanki.Note(
+            model=note_model,
+            fields=[
+                str(nid),
+                html.escape(str(keysig.name(keysig.root))),
+                html.escape(str(keysig.name(note))),
+                str(octave),
+                f'<img src="score_{nid}.png">',
+            ],
+            due=int(nid),
+        )
 
-    note = genanki.Note(
-        model=chord_model,
-        fields=[
-            str(nid),
-            html.escape(str(keysig.name(keysig.root))),
-            html.escape(chord_name),
-            str(inversion),
-            staff,
-            f'<img src="score_{nid}.png">',
-        ],
-        due=int(nid),
-    )
+        deck.add_note(note)
 
-    deck.add_note(note)
+    iterable = itertools.product(roots, (0, 1, 2), ('bass', 'treble'), ChordKind)
+    total = 12 * len(ChordKind) * 3 * 2
+
+    for root, inversion, staff, kind in tqdm.tqdm(iterable, total=total, leave=False, desc="Chords"):
+        nid = format(next(nids), '06d')
+
+        chord = accordion_chord(root, kind)
+        if staff == 'treble':
+            chord = tuple(note + 24 for note in chord)
+
+        chord_name = kind.name(root)
+
+        media_path = f'images/score_{nid}.png'
+        media_files.append(media_path)
+
+        with ScoreRenderer(keysig, media_path) as renderer:
+            renderer.add_chord(invert(inversion, chord), staff)
+
+        note = genanki.Note(
+            model=chord_model,
+            fields=[
+                str(nid),
+                html.escape(str(keysig.name(keysig.root))),
+                html.escape(chord_name),
+                str(inversion),
+                staff,
+                f'<img src="score_{nid}.png">',
+            ],
+            due=int(nid),
+        )
+
+        deck.add_note(note)
 
 package = genanki.Package(deck, media_files)
 package.write_to_file('accordion.apkg')
--- notation_flashcards/render.py
+++ notation_flashcards/render.py
@@ -1,4 +1,6 @@
+from types import TracebackType
 import typing
+import contextlib
 
 from neoscore.common import *
 
@@ -7,50 +9,78 @@ from notation_flashcards import theory
 
 neoscore.setup()
 
-def render_chord(
-    keysig: theory.KeySignature,
-    chord: tuple[theory.Note],
-    staff: typing.Literal['treble', 'bass'],
-    path: str,
-) -> None:
-    group = StaffGroup()
-    width = Mm(20)
 
-    assert keysig.mode in (theory.Mode.MAJOR, theory.Mode.MINOR)
+_score_exists = False
 
-    root_name = keysig.name(keysig.root)
-    keysig_string = ''.join(
-        (
-            root_name.letter.lower(),
-            root_name.accidental.neoscore,
-            '_major' if keysig.mode == theory.Mode.MAJOR else '_minor',
+
+class ScoreRenderer(contextlib.AbstractContextManager):
+    def __init__(self, keysig: KeySignature, path: str) -> None:
+        global _score_exists
+        assert not _score_exists, "Only one Score object can exist at a time"
+
+        assert keysig.mode in (theory.Mode.MAJOR, theory.Mode.MINOR)
+
+        self.keysig = keysig
+        self.path = path
+
+        self.group = StaffGroup()
+
+        width = Mm(20)
+
+        root_name = keysig.name(keysig.root)
+        keysig_string = ''.join(
+            (
+                root_name.letter.lower(),
+                root_name.accidental.neoscore,
+                '_major' if keysig.mode == theory.Mode.MAJOR else '_minor',
+            )
         )
-    )
 
-    treble = Staff(ORIGIN, None, width, group)
-    Clef(ZERO, treble, 'treble')
-    KeySignature(ZERO, treble, keysig_string)
+        self.treble = Staff(ORIGIN, None, width, self.group)
+        Clef(ZERO, self.treble, 'treble')
+        KeySignature(ZERO, self.treble, keysig_string)
 
-    bass = Staff((ZERO, treble.unit(8)), None, width, group)
-    Clef(ZERO, bass, 'bass')
-    KeySignature(ZERO, bass, keysig_string)
+        self.bass = Staff((ZERO, self.treble.unit(8)), None, width, self.group)
+        Clef(ZERO, self.bass, 'bass')
+        KeySignature(ZERO, self.bass, keysig_string)
 
-    Brace(group)
-    SystemLine(group)
+        Brace(self.group)
+        SystemLine(self.group)
 
-    Barline(ZERO, group)
+        Barline(ZERO, self.group)
 
-    if staff == 'treble':
-        staff = treble
-    else:
-        staff = bass
+    def add_note(self, note: theory.Note, staff: typing.Literat['treble', 'bass']) -> None:
+        self.add_chord((note,), staff)
 
-    Chordrest(staff.unit(4), staff, [str(keysig.engrave(note)) for note in chord], (1, 4))
+    def add_chord\
+        ( self
+        , notes: typing.Iterable[theory.Note]
+        , staff: typing.Literal['treble', 'bass']
+        ) -> None:
+        if staff == 'treble':
+            staff = self.treble
+        else:
+            staff = self.bass
 
-    # neoscore forgets to close its file handles, so use bytearray
-    neoscore.render_image(rect=(-Mm(23), -Mm(5), Mm(45), Mm(35)), dest=path, wait=True)
+        Chordrest(staff.unit(4), staff, [str(self.keysig.engrave(note)) for note in notes], (1, 4))
 
-    # reset, but don't tear down Qt (because it throws a bunch of error messages and takes ages to boot)
-    neoscore.document = Document(neoscore.document.paper)
-    neoscore.app_interface.clear_scene()
+    def __enter__(self) -> Any:
+        return super().__enter__()
+    
+    def __exit__\
+        ( self
+        , __exc_type: type[BaseException] | None
+        , __exc_value: BaseException | None
+        , __traceback: TracebackType | None
+        ) -> bool | None:
+        global _score_exists
+        assert _score_exists
 
+        neoscore.render_image(rect=(-Mm(23), -Mm(5), Mm(45), Mm(35)), dest=self.path, wait=True)
+
+        # reset, but don't tear down Qt (because it throws a bunch of error messages and takes ages to boot)
+        neoscore.document = Document(neoscore.document.paper)
+        neoscore.app_interface.clear_scene()
+        _score_exists = False
+
+        return super().__exit__(__exc_type, __exc_value, __traceback)
